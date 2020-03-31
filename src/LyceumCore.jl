290444a34885136d9381d94f8ffdc2f701a0e1bb
colinxs <me@colinxsummers.com>
2020-03-30 20:22:27 -0700

    remove static

--- src/LyceumCore.jl
+++ src/LyceumCore.jl
@@ -11,10 +11,6 @@ export AbsArr, AbsMat, AbsVec
 export AbsNestedArr, AbsSimilarNestedArr
 include("aliases.jl")
 
-export StaticOrVal, SBool, STrue, SFalse
-export wrapval, wrapstatic, unwrap
-include("static.jl")
-
 export front, tail, tuple_split, tuple_length, tuple_prod, tuple_minimum
 include("util.jl")
 
